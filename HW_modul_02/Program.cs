ef3e88dbd731915e8164e0c1f4db1933b0af89bf
1 <1@LAPTOP-8U2CR0NO>
2023-06-10 09:48:05 +0300

    Добавьте файлы проекта.

---
+++ HW_modul_02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_modul_02
+{
+    internal class Program
+    {
+
+         
+            
+
+
+
+        
+
+
+        static void Main(string[] args)
+        {
+            // Дз 1
+            //Console.WriteLine("Введите число в диапозоне от 1  до 100 " );
+            //int a= Convert.ToInt32(Console.ReadLine());
+            //int b= Convert.ToInt32(Console.ReadLine());
+            //if (1>a||a>100 || b>100|| b<1) { Console.WriteLine("Диапозон должен быть строго от 1 до 100 "); }
+            //else if (a%3==0 || b % 3 == 0) { Console.WriteLine("Fizz "); }
+            //else if (a%5==0 || b % 5 == 0) { Console.WriteLine("Buzz "); }
+            //else  { Console.WriteLine($"{a},{b}"); }
+
+            //Дз2
+            //Console.WriteLine(" ВВедите число, а затем %, который необходимо посчитать ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = 0;
+            //Console.WriteLine(a * b / 100);
+
+            //LДз 3
+            // Console.WriteLine(" ВВедите 4 цифры ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{a}{b}{c}{d} ");
+            //Дз4
+            //Console.WriteLine("Введите шестизначное число:");
+            //string numStr = Console.ReadLine();
+
+            //int numInt = Convert.ToInt32(numStr);
+            //if (numInt > 999999 || numInt < 100000) Console.WriteLine("Ошибка. Введите шестизначное число:");
+
+            //Console.WriteLine("Какие цифры поменять местами:");
+            //int numInt1 = Convert.ToInt32(Console.ReadLine());
+            //int numInt2 = Convert.ToInt32(Console.ReadLine());
+
+            //char[] numStrArr = numStr.ToCharArray();
+            //char temp1 = numStrArr[numInt1 - 1];
+            //char temp2 = numStrArr[numInt2 - 1];
+
+            //numStrArr[numInt1 - 1] = temp2;
+            //numStrArr[numInt2 - 1] = temp1;
+
+            //Console.WriteLine(numStrArr);
+            //Д3 5
+            //Console.WriteLine("Введите дату в формате dd.mm.yyyy");
+            //DateTime dt = DateTime.Parse(Console.ReadLine());
+            //Console.WriteLine("Это день недели № {0}", (int)dt.DayOfWeek);
+            //Console.ReadKey();
+            //Д3 6
+        //    Console.WriteLine("Введите диапозон чисел ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    int b = Convert.ToInt32(Console.ReadLine());
+
+        //    if (a > b)
+        //    {
+        //        int temp;
+        //        temp = a;
+        //        a = b;
+        //        b = temp;
+        //    }
+
+        //    for (int i = a; i <= b; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //            Console.WriteLine(i);
+        //    }
+        //}
+    }
+}
